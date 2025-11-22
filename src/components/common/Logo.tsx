a83076013813c392d8e9c9b269e91f36c6d778bb
aissamed7 <contacteinfo71@gmail.com>
2025-11-23 00:43:22 +0300

    feat: Update logo and refactor schedule hook
    Refactors the `useWeeklySchedule` hook from `src/hooks` to `src/utils` for better organization.
    Updates the application logo with a new design featuring Islamic elements.

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -5,13 +5,26 @@ interface LogoProps {
 }
 
 export const Logo: React.FC<LogoProps> = ({ className }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className={className}>
-    <g fill="currentColor">
-      <path d="M210 120c-32 22-52 58-52 98s20 76 52 98c-46-10-82-52-82-98s36-88 82-98z"/>
-      <polygon points="260,150 272,182 306,182 278,202 290,234 260,214 230,234 242,202 214,182 248,182"/>
-      <path d="M330 110h30v260h-30z"/>
-      <path d="M345 70l15 30h-30l15-30z"/>
-      <path d="M420 370H260v-60c0-55 45-100 100-100s100 45 100 100v60h-40zM360 190l10-20 10 20z"/>
-    </g>
+  <svg 
+    xmlns="http://www.w3.org/2000/svg" 
+    viewBox="0 0 200 200" 
+    className={className}
+  >
+    {/* خلفية حمراء بزوايا مستديرة */}
+    <rect x="0" y="0" width="200" height="200" rx="30" fill="#D80027"/>
+
+    {/* الهلال */}
+    <path d="M70 100a30 30 0 1 1 0-60a25 25 0 1 0 0 60z" fill="white"/>
+
+    {/* النجمة */}
+    <polygon points="90,70 94,80 105,80 96,86 100,96 90,90 80,96 84,86 75,80 86,80" fill="white"/>
+
+    {/* قبة المسجد */}
+    <path d="M130 130 a20 20 0 0 1 40 0 v20 h-40z" fill="white"/>
+    <circle cx="150" cy="130" r="3" fill="white"/>
+
+    {/* المئذنة */}
+    <rect x="120" y="90" width="8" height="60" fill="white"/>
+    <path d="M124 80 L128 90 L120 90 Z" fill="white"/>
   </svg>
 );
